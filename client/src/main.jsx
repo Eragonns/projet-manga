d69c48e3fcb99415a16894264361f04c76426cfc
Eragonns <166370297+Eragonns@users.noreply.github.com>
2024-12-20 16:38:16 +0100

    deploiement

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-
 import "../SASS/main.css";
 
 import { AuthProvider } from "./contexts/AuthContext.jsx";
